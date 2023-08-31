d56e1b9fdbdfcae487ad60e9df8c92500c26951a
Asef Islam <asef@cs.stanford.edu>
2023-08-30 23:35:12 -0700

    Table vertical

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pandas as pd
 from dash import Dash, Input, Output, dcc, html, dash_table
 from dash.dependencies import Input, Output
 
-
 # Load in data containing terms
 data = pd.read_csv('input.csv')
 
@@ -35,13 +34,7 @@ app.layout = html.Div(style={'font-family': 'Arial', 'max-width': '800px', 'marg
         ),
         html.Div(id='output-display', style={'margin-top': '20px'}),
         html.H4('Top 10 matching HCA terms', style={'margin-top': '20px'}),
-        dash_table.DataTable(
-            id='matching-terms-table',
-            columns=[{'name': col, 'id': col} for col in data.columns[1:11]],  # Columns 2 to 11
-            data=[],
-            style_table={'margin-top': '10px'},
-            style_cell={'textAlign': 'left'}
-        )
+        html.Div(id='matching-terms-table')  # Empty div for table display
     ])
 ])
 
@@ -49,27 +42,39 @@ app.layout = html.Div(style={'font-family': 'Arial', 'max-width': '800px', 'marg
 # Define the callback to update the output-display and matching-terms-table
 @app.callback(
     [Output('output-display', 'children'),
-     Output('matching-terms-table', 'data')],
+     Output('matching-terms-table', 'children')],
     Input('term-dropdown', 'value')
 )
 def update_output(selected_term):
     if selected_term is None:
-        return html.Div(), []
+        return [], []  # Return empty lists
 
     corresponding_term = data[data.iloc[:, 0] == selected_term].iloc[0, 1]
     match_quality = data[data.iloc[:, 0] == selected_term].iloc[0, 11]
 
     match_style, match_text = get_match_style_and_text(match_quality)
 
-    matching_terms_table_data = [data[data.iloc[:, 0] == selected_term].iloc[0, 1:11].to_dict()]
+    matching_terms_table_data = data[data.iloc[:, 0] == selected_term].iloc[0, 1:11].to_frame(name='Matching Terms')
+    matching_terms_table_data.reset_index(drop=True, inplace=True)
 
-    return html.Div([
-        html.H4(f'Selected HuBMAP term: {selected_term}', style={'margin-bottom': '5px', 'text-align': 'left'}),
+    return [
         html.Div([
-            html.Div('Best HCA Match: ' + corresponding_term, style={'padding': '10px', 'text-align': 'left'}),
-            html.Div(match_text, style={**match_style, 'padding': '10px', 'border-radius': '5px', 'margin-top': '5px'})
-        ], style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'})
-    ], style={'background-color': '#f4f4f4', 'padding': '20px', 'border-radius': '5px'}), matching_terms_table_data
+            html.H4(f'Selected HuBMAP term: {selected_term}', style={'margin-bottom': '5px', 'text-align': 'left'}),
+            html.Div([
+                html.Div('Best HCA Match: ' + corresponding_term, style={'padding': '10px', 'text-align': 'left'}),
+                html.Div(match_text,
+                         style={**match_style, 'padding': '10px', 'border-radius': '5px', 'margin-top': '5px'})
+            ], style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'}),
+        ], style={'background-color': '#f4f4f4', 'padding': '20px', 'border-radius': '5px'}),
+
+        dash_table.DataTable(
+            id='matching-terms-table-display',
+            columns=[{'name': 'Matching Terms', 'id': 'Matching Terms'}],
+            data=matching_terms_table_data.to_dict('records'),
+            style_table={'margin-top': '10px'},
+            style_cell={'textAlign': 'left', 'padding': '8px'}
+        )
+    ]
 
 
 # Run the app
