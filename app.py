9948f489efbed2897b0339e29360280f6f873349
Asef Islam <asef@cs.stanford.edu>
2023-08-30 23:30:23 -0700

    Color coded match quality plus top 10 terms

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from dash import Dash, Input, Output, dcc, html
+from dash import Dash, Input, Output, dcc, html, dash_table
+from dash.dependencies import Input, Output
 
 
 # Load in data containing terms
@@ -8,9 +9,23 @@ data = pd.read_csv('input.csv')
 # Initialize the Dash app
 app = Dash(__name__)
 
+# Set the page title
+app.title = 'HuBMAP to HCA term'
+
+
+# Function to determine style and text based on match quality
+def get_match_style_and_text(match_quality):
+    if match_quality == 'yes':
+        return {'background-color': 'green', 'color': 'white'}, 'This is a good match'
+    elif match_quality == 'no':
+        return {'background-color': 'red', 'color': 'white'}, 'This is a bad match'
+    else:
+        return {'background-color': 'yellow'}, 'Match quality unknown'
+
+
 # Define the style of the app
-app.layout = html.Div(style={'font-family': 'Arial', 'max-width': '600px', 'margin': 'auto'}, children=[
-    html.H1('Interactive Term Dashboard', style={'text-align': 'center'}),
+app.layout = html.Div(style={'font-family': 'Arial', 'max-width': '800px', 'margin': 'auto'}, children=[
+    html.H1('HuBMAP to HCA term', style={'text-align': 'center'}),
     html.Div([
         dcc.Dropdown(
             id='term-dropdown',
@@ -18,196 +33,45 @@ app.layout = html.Div(style={'font-family': 'Arial', 'max-width': '600px', 'marg
             value=None,
             placeholder='Select a term'
         ),
-        html.Div(id='output-display', style={'margin-top': '20px'})
+        html.Div(id='output-display', style={'margin-top': '20px'}),
+        html.H4('Top 10 matching HCA terms', style={'margin-top': '20px'}),
+        dash_table.DataTable(
+            id='matching-terms-table',
+            columns=[{'name': col, 'id': col} for col in data.columns[1:11]],  # Columns 2 to 11
+            data=[],
+            style_table={'margin-top': '10px'},
+            style_cell={'textAlign': 'left'}
+        )
     ])
 ])
 
-# Define the callback to update the output-display
+
+# Define the callback to update the output-display and matching-terms-table
 @app.callback(
-    Output('output-display', 'children'),
+    [Output('output-display', 'children'),
+     Output('matching-terms-table', 'data')],
     Input('term-dropdown', 'value')
 )
 def update_output(selected_term):
     if selected_term is None:
-        return html.Div()
+        return html.Div(), []
 
     corresponding_term = data[data.iloc[:, 0] == selected_term].iloc[0, 1]
+    match_quality = data[data.iloc[:, 0] == selected_term].iloc[0, 11]
+
+    match_style, match_text = get_match_style_and_text(match_quality)
+
+    matching_terms_table_data = [data[data.iloc[:, 0] == selected_term].iloc[0, 1:11].to_dict()]
+
     return html.Div([
-        html.H4(f'Selected Term: {selected_term}', style={'margin-bottom': '5px'}),
-        html.P(f'Corresponding Term: {corresponding_term}')
-    ], style={'background-color': '#f4f4f4', 'padding': '10px', 'border-radius': '5px'})
+        html.H4(f'Selected HuBMAP term: {selected_term}', style={'margin-bottom': '5px', 'text-align': 'left'}),
+        html.Div([
+            html.Div('Best HCA Match: ' + corresponding_term, style={'padding': '10px', 'text-align': 'left'}),
+            html.Div(match_text, style={**match_style, 'padding': '10px', 'border-radius': '5px', 'margin-top': '5px'})
+        ], style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'})
+    ], style={'background-color': '#f4f4f4', 'padding': '20px', 'border-radius': '5px'}), matching_terms_table_data
+
 
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-# data = (
-#     pd.read_csv("avocado.csv")
-#     .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
-#     .sort_values(by="Date")
-# )
-# regions = data["region"].sort_values().unique()
-# avocado_types = data["type"].sort_values().unique()
-#
-# external_stylesheets = [
-#     {
-#         "href": (
-#             "https://fonts.googleapis.com/css2?"
-#             "family=Lato:wght@400;700&display=swap"
-#         ),
-#         "rel": "stylesheet",
-#     },
-# ]
-# app = Dash(__name__, external_stylesheets=external_stylesheets)
-# app.title = "Avocado Analytics: Understand Your Avocados!"
-#
-# app.layout = html.Div(
-#     children=[
-#         html.Div(
-#             children=[
-#                 html.P(children="🥑", className="header-emoji"),
-#                 html.H1(
-#                     children="Avocado Analytics", className="header-title"
-#                 ),
-#                 html.P(
-#                     children=(
-#                         "Analyze the behavior of avocado prices and the number"
-#                         " of avocados sold in the US between 2015 and 2018"
-#                     ),
-#                     className="header-description",
-#                 ),
-#             ],
-#             className="header",
-#         ),
-#         html.Div(
-#             children=[
-#                 html.Div(
-#                     children=[
-#                         html.Div(children="Region", className="menu-title"),
-#                         dcc.Dropdown(
-#                             id="region-filter",
-#                             options=[
-#                                 {"label": region, "value": region}
-#                                 for region in regions
-#                             ],
-#                             value="Albany",
-#                             clearable=False,
-#                             className="dropdown",
-#                         ),
-#                     ]
-#                 ),
-#                 html.Div(
-#                     children=[
-#                         html.Div(children="Type", className="menu-title"),
-#                         dcc.Dropdown(
-#                             id="type-filter",
-#                             options=[
-#                                 {
-#                                     "label": avocado_type.title(),
-#                                     "value": avocado_type,
-#                                 }
-#                                 for avocado_type in avocado_types
-#                             ],
-#                             value="organic",
-#                             clearable=False,
-#                             searchable=False,
-#                             className="dropdown",
-#                         ),
-#                     ],
-#                 ),
-#                 html.Div(
-#                     children=[
-#                         html.Div(
-#                             children="Date Range", className="menu-title"
-#                         ),
-#                         dcc.DatePickerRange(
-#                             id="date-range",
-#                             min_date_allowed=data["Date"].min().date(),
-#                             max_date_allowed=data["Date"].max().date(),
-#                             start_date=data["Date"].min().date(),
-#                             end_date=data["Date"].max().date(),
-#                         ),
-#                     ]
-#                 ),
-#             ],
-#             className="menu",
-#         ),
-#         html.Div(
-#             children=[
-#                 html.Div(
-#                     children=dcc.Graph(
-#                         id="price-chart",
-#                         config={"displayModeBar": False},
-#                     ),
-#                     className="card",
-#                 ),
-#                 html.Div(
-#                     children=dcc.Graph(
-#                         id="volume-chart",
-#                         config={"displayModeBar": False},
-#                     ),
-#                     className="card",
-#                 ),
-#             ],
-#             className="wrapper",
-#         ),
-#     ]
-# )
-#
-#
-# @app.callback(
-#     Output("price-chart", "figure"),
-#     Output("volume-chart", "figure"),
-#     Input("region-filter", "value"),
-#     Input("type-filter", "value"),
-#     Input("date-range", "start_date"),
-#     Input("date-range", "end_date"),
-# )
-# def update_charts(region, avocado_type, start_date, end_date):
-#     filtered_data = data.query(
-#         "region == @region and type == @avocado_type"
-#         " and Date >= @start_date and Date <= @end_date"
-#     )
-#     price_chart_figure = {
-#         "data": [
-#             {
-#                 "x": filtered_data["Date"],
-#                 "y": filtered_data["AveragePrice"],
-#                 "type": "lines",
-#                 "hovertemplate": "$%{y:.2f}<extra></extra>",
-#             },
-#         ],
-#         "layout": {
-#             "title": {
-#                 "text": "Average Price of Avocados",
-#                 "x": 0.05,
-#                 "xanchor": "left",
-#             },
-#             "xaxis": {"fixedrange": True},
-#             "yaxis": {"tickprefix": "$", "fixedrange": True},
-#             "colorway": ["#17B897"],
-#         },
-#     }
-#
-#     volume_chart_figure = {
-#         "data": [
-#             {
-#                 "x": filtered_data["Date"],
-#                 "y": filtered_data["Total Volume"],
-#                 "type": "lines",
-#             },
-#         ],
-#         "layout": {
-#             "title": {"text": "Avocados Sold", "x": 0.05, "xanchor": "left"},
-#             "xaxis": {"fixedrange": True},
-#             "yaxis": {"fixedrange": True},
-#             "colorway": ["#E12D39"],
-#         },
-#     }
-#     return price_chart_figure, volume_chart_figure
-#
-#
-# if __name__ == "__main__":
-#     app.run_server(debug=True)
